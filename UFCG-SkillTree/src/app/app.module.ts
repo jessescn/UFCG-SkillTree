d8373a92374279839f570ea2a541639227829044
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2019-03-23 13:21:07 -0300

    Added d3 base graph. Now and then is necessary adaptation and remove force directives

--- UFCG-SkillTree/src/app/app.module.ts
+++ UFCG-SkillTree/src/app/app.module.ts
@@ -8,6 +8,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { MenubarComponent } from './menubar/menubar.component';
 import { ProfileBarComponent } from './profile-bar/profile-bar.component';
 
+import { D3Service, D3_DIRECTIVES } from './graph/d3';
+
+import { GraphComponent } from './graph/visuals/graph/graph.component';
+import { SHARED_VISUALS } from './graph/visuals/shared';
+
+
 const appRoutes: Routes = [
   { path:'./', component: AppComponent },
   { path: 'beehive', component: beeHiveGraphComponent }
@@ -18,14 +24,18 @@ const appRoutes: Routes = [
     AppComponent,
     beeHiveGraphComponent,
     MenubarComponent,
-    ProfileBarComponent
+    ProfileBarComponent,
+    ...SHARED_VISUALS,
+    ...D3_DIRECTIVES,
+    GraphComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes, { enableTracing: true})
   ],
-  providers: [],
+  providers: [ D3Service ],
   bootstrap: [AppComponent]
 })
 export class AppModule { 
