7f163d2b31cdb60cdf81bf5a0ba0fd64c5cfa786
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2019-04-01 16:05:40 -0300

    Removed beehive example

--- UFCG-SkillTree/src/app/app.module.ts
+++ UFCG-SkillTree/src/app/app.module.ts
@@ -3,18 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { beeHiveGraphComponent } from './beehive-graph/beehive-graph.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path:'./', component: AppComponent },
-  { path: 'beehive', component: beeHiveGraphComponent }
+  { path:'./', component: AppComponent }
 ]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    beeHiveGraphComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
